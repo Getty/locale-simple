747287b6ffa0bd864686f24d56d5625cc9281566
Torsten Raudssus <torsten@raudss.us>
2026-04-22 06:36:02 +0200

    increment $VERSION after 0.108 release

--- lib/Locale/Simple.pm
+++ lib/Locale/Simple.pm
@@ -1,6 +1,6 @@
 package Locale::Simple;
 # ABSTRACT: Functions for translate text based on gettext data, also in JavaScript
-our $VERSION = '0.108';
+our $VERSION = '0.109';
 use strict;
 use warnings;
 
